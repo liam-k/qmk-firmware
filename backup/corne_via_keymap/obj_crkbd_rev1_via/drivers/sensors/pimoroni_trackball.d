522c0c827fb0a01204a0bff13f34b4ddff86f1e0
liam-k <liamk@pm.me>
2025-05-06 15:32:02 +0200

    chore: backup built firmware before chordal update

---
+++ backup/corne_via_keymap/obj_crkbd_rev1_via/drivers/sensors/pimoroni_trackball.d
@@ -0,0 +1,107 @@
+.build/obj_crkbd_rev1_via/drivers/sensors/pimoroni_trackball.o: \
+ drivers/sensors/pimoroni_trackball.c \
+ .build/obj_crkbd_rev1_via/src/info_config.h users/idank/config.h \
+ keyboards/crkbd/keymaps/via/config.h \
+ platforms/chibios/boards/GENERIC_PROMICRO_RP2040/configs/config.h \
+ platforms/chibios/config.h keyboards/crkbd/post_config.h \
+ platforms/chibios/drivers/wear_leveling/wear_leveling_rp2040_flash_config.h \
+ lib/pico-sdk/src/rp2_common/hardware_flash/include/hardware/flash.h \
+ lib/pico-sdk/src/common/pico_base/include/pico.h \
+ lib/pico-sdk/src/common/pico_base/include/pico/types.h \
+ lib/pico-sdk/src/common/pico_base/include/pico/assert.h \
+ lib/chibios//os/various/pico_bindings/dumb/include/pico/version.h \
+ lib/pico-sdk/src/common/pico_base/include/pico/config.h \
+ lib/chibios//os/various/pico_bindings/dumb/include/pico/config_autogen.h \
+ lib/pico-sdk/src/boards/include/boards/pico.h \
+ lib/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h \
+ lib/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ lib/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ lib/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ lib/pico-sdk/src/common/pico_base/include/pico/types.h \
+ lib/pico-sdk/src/common/pico_base/include/pico/error.h \
+ quantum/rgblight/rgblight_post_config.h \
+ quantum/split_common/post_config.h quantum/pointing_device_internal.h \
+ quantum/logging/debug.h quantum/logging/print.h quantum/util.h \
+ quantum/bitwise.h platforms/chibios/_util.h quantum/logging/sendchar.h \
+ platforms/progmem.h drivers/sensors/pimoroni_trackball.h \
+ tmk_core/protocol/report.h quantum/keycode.h quantum/keycodes.h \
+ quantum/modifiers.h platforms/chibios/drivers/i2c_master.h \
+ platforms/timer.h platforms/chibios/_timer.h
+
+.build/obj_crkbd_rev1_via/src/info_config.h:
+
+users/idank/config.h:
+
+keyboards/crkbd/keymaps/via/config.h:
+
+platforms/chibios/boards/GENERIC_PROMICRO_RP2040/configs/config.h:
+
+platforms/chibios/config.h:
+
+keyboards/crkbd/post_config.h:
+
+platforms/chibios/drivers/wear_leveling/wear_leveling_rp2040_flash_config.h:
+
+lib/pico-sdk/src/rp2_common/hardware_flash/include/hardware/flash.h:
+
+lib/pico-sdk/src/common/pico_base/include/pico.h:
+
+lib/pico-sdk/src/common/pico_base/include/pico/types.h:
+
+lib/pico-sdk/src/common/pico_base/include/pico/assert.h:
+
+lib/chibios//os/various/pico_bindings/dumb/include/pico/version.h:
+
+lib/pico-sdk/src/common/pico_base/include/pico/config.h:
+
+lib/chibios//os/various/pico_bindings/dumb/include/pico/config_autogen.h:
+
+lib/pico-sdk/src/boards/include/boards/pico.h:
+
+lib/pico-sdk/src/rp2_common/pico_platform/include/pico/platform.h:
+
+lib/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h:
+
+lib/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h:
+
+lib/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h:
+
+lib/pico-sdk/src/common/pico_base/include/pico/types.h:
+
+lib/pico-sdk/src/common/pico_base/include/pico/error.h:
+
+quantum/rgblight/rgblight_post_config.h:
+
+quantum/split_common/post_config.h:
+
+quantum/pointing_device_internal.h:
+
+quantum/logging/debug.h:
+
+quantum/logging/print.h:
+
+quantum/util.h:
+
+quantum/bitwise.h:
+
+platforms/chibios/_util.h:
+
+quantum/logging/sendchar.h:
+
+platforms/progmem.h:
+
+drivers/sensors/pimoroni_trackball.h:
+
+tmk_core/protocol/report.h:
+
+quantum/keycode.h:
+
+quantum/keycodes.h:
+
+quantum/modifiers.h:
+
+platforms/chibios/drivers/i2c_master.h:
+
+platforms/timer.h:
+
+platforms/chibios/_timer.h:
